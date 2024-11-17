8d52ae38b31c11e12d31bb0ca6a3b2dde8ca290f
Aroshana Dissanayake <aroshana6@gmail.com>
2024-11-17 20:13:01 +0530

    landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
-  return <h1>Landing</h1>;
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} className="logo" alt="topjobs" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Find your <span>Dream</span> Job
+          </h1>
+          <p>
+            We provide a platform where you can find your dream job. We have
+            thousands of job listings from different companies.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login/Demo User
+          </Link>
+        </div>
+        <img src={main} className="main main-img" alt="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Landing;
