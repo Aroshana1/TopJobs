43fc970f55cb980144fb3167c0be350386dd3e80
Aroshana Dissanayake <aroshana6@gmail.com>
2024-11-19 23:40:39 +0530

    reg

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Wrapper } from "../assets/wrappers/RegisterAndLoginPage";
+import { Logo } from "../components";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to="/login">Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+      </form>
+    </Wrapper>
   );
 };
 
