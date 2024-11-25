d1a93b721916d7d95e59a962a1aa4e934086d1f3
Aroshana Dissanayake <aroshana6@gmail.com>
2024-11-25 23:01:50 +0530

    register and login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { Logo, FormRow } from "../components";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <Link to="/register">Register Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Login</h4>
+        <FormRow type="email" name="email" defaultValue="John@email.com" />
+        <FormRow type="password" name="password" defaultValue="password" />
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <button type="button" className="btn btn-block">
+          Explore Jobs
+        </button>
+        <p>
+          Not a member yet?
+          <Link to="/Register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { Logo } from "../components";
+import { FormRow } from "../components";
 
 const Register = () => {
   return (
@@ -9,6 +10,25 @@ const Register = () => {
       <form className="form">
         <Logo />
         <h4>Register</h4>
+        <FormRow type="text" name="name" defaultValue="John" />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="Last Name"
+          defaultValue="Smith"
+        />
+        <FormRow type="text" name="location" defaultValue="Earth" />
+        <FormRow type="email" name="email" defaultValue="John@email.com" />
+        <FormRow type="password" name="password" defaultValue="password" />
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <p>
+          Already a member?
+          <Link to="/Login" className="member-btn">
+            Login
+          </Link>
+        </p>
       </form>
     </Wrapper>
   );
